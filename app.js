80e593451f69aebeb7b4127a677cf0ef74f908a1
Sebastian Fuss <sebastian.fuss@googlemail.com>
2018-10-31 13:07:00 +0100

    Update deps, use EcmaScript 6

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+const routes = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -40,23 +38,23 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,47 @@
-var url = require('url');
+const url = require('url');
 
-var express = require('express');
-var _ = require('lodash');
+const express = require('express');
+const debug = require('debug')('delayla:server');
+const _ = require('lodash');
 
-var router = express.Router();
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    var viewData = {url: req.protocol + '://' + req.get('host')};
-    if (req.query.url && req.query.delay) {
-        viewData.delayUrl = viewData.url + '/delay/' + encodeURIComponent(req.query.url) + '/for/' + req.query.delay;
-    }
-    res.render('index', viewData);
+// GET home page.
+router.get('/', (req, res) => {
+  const viewData = { url: `${req.protocol}://${req.get('host')}` };
+  if (req.query.url && req.query.delay) {
+    viewData.delayUrl = `${viewData.url}/delay/${encodeURIComponent(req.query.url)}/for/${req.query.delay}`;
+  }
+  res.render('index', viewData);
 });
 
-var delalay = function (req, res, next) {
-    // get the delay which should be between 0 and 10 seconds
-    var delay = Math.max(0, Math.min(parseInt(req.params.delay, 10), 10));
+const delalay = (req, res) => {
+  // get the delay which should be between 0 and 10 seconds
+  const delay = Math.max(0, Math.min(parseInt(req.params.delay, 10), 10));
 
-    var givenUrl = req.params.url;
+  const givenUrl = req.params.url;
 
-    if (!givenUrl) {
-        res.sendStatus(400);
-        return;
-    }
+  if (!givenUrl) {
+    res.sendStatus(400);
+    return;
+  }
 
-    var givenUrlObj = url.parse(givenUrl, true);
-    var requestUrlObj = url.parse(req.url, true);
+  const givenUrlObj = url.parse(givenUrl, true);
+  const requestUrlObj = url.parse(req.url, true);
 
-    var targetUrlObj = givenUrlObj;
+  const targetUrlObj = givenUrlObj;
 
-    if (requestUrlObj.query) {
-        targetUrlObj.search = undefined; // Set search property to null to force creation of it from the query object
-        targetUrlObj.query = _.merge(targetUrlObj.query, requestUrlObj.query);
-    }
+  if (requestUrlObj.query) {
+    // Set search property to null to force creation of it from the query object
+    targetUrlObj.search = undefined;
+    targetUrlObj.query = _.merge(targetUrlObj.query, requestUrlObj.query);
+  }
 
-    var targetUrl = url.format(targetUrlObj);
-    setTimeout(function () {
-        console.log('Now redirecting to ' + targetUrl);
-        res.redirect(targetUrl);
-    }, delay * 1000);
+  const targetUrl = url.format(targetUrlObj);
+  setTimeout(() => {
+    debug(`Now redirecting to ${targetUrl}`);
+    res.redirect(targetUrl);
+  }, delay * 1000);
 };
 
 router.get('/delay/:url/for/:delay', delalay);
